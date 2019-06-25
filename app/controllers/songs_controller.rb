397d74aacba4543fd7131a94ac8e7cb8f933dc93
dclements9 <dclements@protonmail.com>
2019-06-25 08:24:10 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,7 @@
 class SongsController < ApplicationController
+  before_action :set_preferences, only: [:new]
+
+
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
@@ -25,8 +28,12 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
-  end
+    if @preferences && !@preferences.allow_create_songs
+      redirect_to songs_path
+    else
+      @song = Song.new
+    end
+end
 
   def create
     @song = Song.new(song_params)
@@ -66,5 +73,9 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title, :artist_name)
   end
-end
 
+
+  def set_preferences
+    @preferences = Preference.first
+  end
+end
