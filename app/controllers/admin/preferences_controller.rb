37bd08977fc9231d7246fad6234900d3f7d03e7d
imaglazer <avigayilglazer@gmail.com>
2019-07-03 16:49:14 +0300

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,6 @@
+class Admin::PreferencesController < ApplicationController
+    def index
+       @preference = Preference.first_or_create(artist_sort_order: "DESC", song_sort_order: "ASC", allow_create_artists: true, allow_create_songs: true)
+    end
+
+end
